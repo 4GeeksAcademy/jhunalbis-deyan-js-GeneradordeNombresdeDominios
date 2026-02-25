7b4bb878083d2a1b3aed07aa1f305617f9fe148d
jhunadev <jhunalbisdeyanquintero@gmail.com>
2026-02-25 15:59:26 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+for (let i = 0; i < pronoun.length; i++) {
+    
+    for (let j = 0; j < adj.length; j++) {
+        
+        for (let k = 0; k < noun.length; k++) {
+            
+            let dominio = pronoun[i] + adj[j] + noun[k] + ".com";
+            
+            console.log(dominio);
+        }
+    }
+}
+
+
   console.log("Hello Rigo from the console!");
 };
